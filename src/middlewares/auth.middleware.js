a979e4c0df654daf54013bb1e73ba29a36a32c6a
tejaspatil3 <tejaspatil1216@gmail.com>
2025-08-13 00:31:13 +0530

    day-10

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,16 +5,18 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 export const verifyJWT = asyncHandler(async(req,_,next) => {
     // token stored in cookies for desktop
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("bearer", "")
-    if(!token){
-        throw new ApiError(401, "Unauthorized")
-    }
     try{
+        const token = req.cookies.accessToken || req.header("Authorization")?.replace("bearer", "")
+        
+        if(!token){
+            throw new ApiError(401, "Unauthorized Request")
+        }    
         const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
 
         if(!user){
-            throw new ApiError(401, "Unauthorized")
+            throw new ApiError(401, "Unauthorized,Invalid Access Token")
         }
         // add more info
         req.user = user;
